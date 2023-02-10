553fa48f6ef297e54c0d5fe45c8c453ae0d56780
aucshow-wjb <110958823+aucshow-wjb@users.noreply.github.com>
2023-02-10 13:54:54 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+//1
+
 function generatePattern(charSet, patternLength) {
   let results = [];
   
@@ -21,3 +29,31 @@ let charSet = "abcdefghijklmnopqrstuvwxyz";
 let patternLength = 4;
 let patterns = generatePattern(charSet, patternLength);
 console.log(patterns);
+
+
+
+
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+//2
+
+function generateString(charSet, index, currentString) {
+    if (index === charSet.length) {
+        console.log(currentString);
+        return;
+    }
+  
+    for (let i = charSet.length - 1; i >= 0; i--) {
+        let newString = currentString + charSet[i];
+        if (!currentString.includes(charSet[i])) {
+            generateString(charSet, index + 1, newString);
+        }
+    }
+}
+
+let charSet = "abc";
+generateString(charSet, 0, "");
